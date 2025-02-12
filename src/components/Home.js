4f30d27cae93937956a6fdec8397065b56e430dc
Ash <ash.wy.aung@gmail.com>
2025-02-11 21:22:31 -0800

    Add placeholder components for all routes

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm [Your Name]. This site is under construction.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+// src/components/Projects.js
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default Projects;
