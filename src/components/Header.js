bec67727849f24f970decdb9e7682ecc39015ea9
Ash <ash.wy.aung@gmail.com>
2025-02-12 03:28:50 -0800

    Added Header component with navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import favicon from '../assets/favicon.ico';
+
 
 function Header() {
   return (
     <header className="header">
       <div className="header-content">
-        {/* Your name or logo */}
         <Link to="/" className="logo">
+        <img src={favicon} alt="Logo" className="favicon" /> {/* New: Added favicon image */}
           Ash Aung
         </Link>
 
         {/* Navigation menu */}
         <nav className="navigation">
           <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>  {/* New Link */}
           <Link to="/projects" className="nav-link">Projects</Link>
           <Link to="/skills" className="nav-link">Skills</Link>
           <Link to="/contact" className="nav-link">Contact</Link>
