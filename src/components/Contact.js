5eab2f2214401b7248cd458228a9780f0af3fca1
Ash <ash.wy.aung@gmail.com>
2025-02-12 05:35:45 -0800

    Created Contact Page with email, LinkedIn, and GitHub links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,16 @@
 // src/components/Contact.js
 import React from 'react';
+import '../styles/Contact.css';
 
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <p>Coming soon...</p>
+      <p>You can reach out to me via email or through my social profiles below.</p>  {/* Updated: Contact message */}
+      <div className="contact-info">  {/* New: Added contact info */}
+        <p>Email: <a href="mailto:hwa201@sfu.ca">hwa201@sfu.ca</a></p>  {/* New: Added email link */}
+        <p>LinkedIn: <a href="https://linkedin.com/in/ash2aung" target="_blank" rel="noopener noreferrer">linkedin.com/in/ash2aung</a></p>  {/* New: Added LinkedIn link */}
+        <p>GitHub: <a href="https://github.com/ash2aung" target="_blank" rel="noopener noreferrer">github.com/ash2aung</a></p>  {/* New: Added GitHub link */}
+      </div>
     </div>
   );
 }
