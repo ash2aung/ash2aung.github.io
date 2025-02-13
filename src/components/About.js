173088b17e2add9066724d92685ec15f6c38b6ea
Ash <ash.wy.aung@gmail.com>
2025-02-12 22:45:57 -0800

    added about me page

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/About.css';
+
+function About() {
+  return (
+    <section className="about-container">
+      <h1>About Me</h1>
+      <p>Hello! My name is Htet Wai Yan Aung, but I usually go by Ash. I'm an 19-year-old student currently studying Software Systems at Simon Fraser University. My hobbies vary from reading manga in bed to going on extreme hikes without food for the entire day. I'm also a huge fan of R&B music, and I've been listening to BRB's albums on repeat for the past few weeks.</p>
+      
+      <h2>Education</h2>
+      <p>I graduated from Yorkville secondary high school in 2023 and began university that same year. I'm currently in my second year, majoring in Software Systems.</p>
+      
+      <h2>Experience</h2>
+      <ul>
+        <li>Worked part-time as a cashier at a stationery store (2016-2023)</li>
+        <li>Worked part-time as a salesman at a snack shop (2015-2020)</li>
+        <li>Volunteered at a church and taught the kids basic English and math (2022)</li>
+        <li>Volunteered at a retirement home to assist elders with their daily needs (2022)</li>
+        <li>Currently the Director of Internal Relations of the Software Systems Student Society (2024-current)</li>
+      </ul>
+      
+      <h2>Hobbies & Interests</h2>
+      <ul>
+        <li>Music: I enjoy listening to R&B, and my favorite artist right now is BRB.</li>
+        <li>Sports: I used to play basketball, but now I mostly play badminton.</li>
+        <li>Travel: I have been to Bangkok, Singapore, Hong Kong, Macau, Indonesia, Malaysia, and Vietnam with my family.</li>
+        <li>Hiking: I started hiking last spring, and I've gotten lost in the mountains a couple of times where I found my way back only after the sunset.</li>
+        <li>Photography: I enjoy photography, especially capturing beaches and mountains. Although I've only used my phone for photography so far, I take great pride in my work. I'm also planning to get a vintage digital camera to explore more.</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
