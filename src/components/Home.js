41b1e1af9616e533773ac362cb8b1259fd90030b
Ash <ash.wy.aung@gmail.com>
2025-02-12 22:14:51 -0800

    changed the layout of home page and added buttons to other pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Home.css";
 import profileImage from "../assets/profile.jpg";
 
@@ -7,7 +8,9 @@ function Home() {
     <section className="home-container">
       <div className="home-content">
         <div className="left-content">
-          <p className="intro-text">I'm a second year Software Systems student at Simon Fraser University.</p>
+          <p className="intro-text">
+            I'm a second year Software Systems student at Simon Fraser University.
+          </p>
           <hr className="divider" />
           <div className="social-links">
             <a href="https://github.com/ash2aung" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -17,13 +20,17 @@ function Home() {
         </div>
         <div className="profile-image-container">
           <img src={profileImage} alt="Ash Aung" className="profile-image" />
+          <div className="name-container">
+            <h1 className="name">Ash Aung</h1>
+            <p className="nickname">(but you can call me Ash)</p>
+          </div>
         </div>
-        <div className="right-content">
-          <h1 className="name">
-            Htet Wai Yan <br /> Aung
-          </h1>
-          <p className="nickname">(but you can call me Ash)</p>
-        </div>
+      </div>
+      <div className="navigation-buttons">
+        <Link to="/about" className="nav-button">About Me</Link>
+        <Link to="/projects" className="nav-button">Projects</Link>
+        <Link to="/skills" className="nav-button">Skills</Link>
+        <Link to="/contact" className="nav-button">Contact</Link>
       </div>
     </section>
   );
