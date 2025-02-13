8b3103385fb0a85563fe071d70eecc7b8215c9e9
Ash <ash.wy.aung@gmail.com>
2025-02-13 03:07:19 -0800

    fixed the issue of wrong favicon being displayed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react'; // Added useEffect to the import
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';  // Added Routes and Route
+import favicon from './assets/favicon.ico'; // Added favicon import
 
 // Import our components
 import Header from './components/Header';
@@ -15,6 +16,13 @@ import Contact from './components/Contact';
 import './styles/App.css';
 
 function App() {
+  useEffect(() => {
+    // Set favicon
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = favicon;
+    document.head.appendChild(link);
+  }, []);
   return (
     <Router>
       <div className="app-container">
